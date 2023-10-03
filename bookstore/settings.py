04ac835066c2cd6471854eab89de67efd5bbc87f
Dennis Ngeno <dngeno@songa.app>
2023-10-03 11:48:53 +0300

    - add allowed hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m*ker$$xm3wzf+i%3(i*4*4bsjyv=+q)i@iq(xe+z$*mr#$&-c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookstore-sx11.onrender.com']
 
 
 # Application definition
